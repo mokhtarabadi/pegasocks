7e51669de256024a21f29c72c1761879f6925af9
Mohammad Reza Mokhtarabadi <mmokhtarabadi@gmail.com>
2021-10-19 10:23:11 +0330

    auto copy generated files

--- compile-android.sh
+++ compile-android.sh
@@ -5,7 +5,7 @@ export WORKING_DIRECTORY=$(pwd)/android
 
 . ./universal-android-toolchain/toolchain.sh "$@"
 
-build_libevent "OFF" "ON"
+#build_libevent "OFF" "ON"
 
 cd $CURRENT_DIR/../
 rm -rf build && mkdir build && cd build || exit
@@ -20,14 +20,20 @@ android_cmake_command \
 "$CMAKE/bin/cmake" --build . --target install
 
 # I know it's ugly, but works!
-PEGASOCKS_ANDROID_CPP_DIR=$HOME/Develop/Projects/pegasocks-android/app/src/main/cpp/prebuilt/
+PEGASOCKS_ANDROID_CPP_DIR="$HOME/Develop/Projects/pegasocks-android/app/src/main/cpp/prebuilt/"
 
-mkdir -p $PEGASOCKS_ANDROID_CPP_DIR/include/$ABI/event2
-mkdir -p $PEGASOCKS_ANDROID_CPP_DIR/lib/$ABI
+if [ -d "$PEGASOCKS_ANDROID_CPP_DIR" ]; then
+  mkdir -p "$PEGASOCKS_ANDROID_CPP_DIR/include/$ABI/event2"
+  mkdir -p "$PEGASOCKS_ANDROID_CPP_DIR/lib/$ABI"
 
-rsync -av $SYSROOT/usr/include/ $PEGASOCKS_ANDROID_CPP_DIR/include
-mv $PEGASOCKS_ANDROID_CPP_DIR/include/event2/event-config.h $PEGASOCKS_ANDROID_CPP_DIR/include/$ABI/event2
+  rsync -av "$SYSROOT/usr/include/" "$PEGASOCKS_ANDROID_CPP_DIR/include"
+  mv "$PEGASOCKS_ANDROID_CPP_DIR/include/event2/event-config.h" "$PEGASOCKS_ANDROID_CPP_DIR/include/$ABI/event2"
 
-cp -r $SYSROOT/usr/lib/libpegas.a $PEGASOCKS_ANDROID_CPP_DIR/lib/$ABI
-cp -r $SYSROOT/usr/lib/libevent{,_mbedtls}.a $PEGASOCKS_ANDROID_CPP_DIR/lib/$ABI
-cp -r $SYSROOT/usr/lib/libmbed*.a $PEGASOCKS_ANDROID_CPP_DIR/lib/$ABI
+  LIBS=(
+    "$SYSROOT/usr/lib/libpegas.a"
+    "$SYSROOT/usr/lib/libevent"{,_mbedtls}.a
+    "$SYSROOT/usr/lib/libmbed"*.a
+  )
+
+  cp -v "${LIBS[@]}" "$PEGASOCKS_ANDROID_CPP_DIR/lib/$ABI"
+fi
